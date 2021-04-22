9397266f7b91276994149cbb6f624bbaf9868655
zefeng <jizefeng0810@163.com>
2021-04-22 14:21:25 +0800

    version20210422

--- run_train.sh
+++ run_train.sh
@@ -20,8 +20,8 @@ python train.py --sdata ./data/cityscapes.yaml --tdata ./data/foggy_cityllscapes
 python train.py --sdata ./data/cityscapes_fake.yaml --tdata ./data/foggy_cityscapes.yaml --epochs 200 --batch-size 32 --weights ./weights/yolov5s.pt --cfg yolos.yaml
 python train.py --sdata ./data/cityscapes_fake.yaml --tdata ./data/foggy_cityllscapes.yaml --epochs 200 --batch-size 32 --weights runs/train/exp83/weights/last.pt --cfg yolos.yaml --resume
 
-python train.py --sdata ./data/bdd_daytime.yaml --tdata ./data/bdd_night.yaml --epochs 200 --batch-size 32 --weights ./weights/yolov5s.pt --cfg yolos.yaml
-python train.py --sdata ./data/bdd_daytime.yaml --tdata ./data/bdd_night.yaml --epochs 200 --batch-size 32 --weights runs/train/exp88/weights/last.pt --cfg yolos.yaml --resume
+python train.py --sdata ./data/bdd_daytime.yaml --tdata ./data/bdd_night.yaml --epochs 200 --batch-size 32 --weights ./weights/yolov5s.pt --cfg yolos.yaml --hyp data/hyp.scratch_bdd.yaml
+python train.py --sdata ./data/bdd_daytime.yaml --tdata ./data/bdd_night.yaml --epochs 200 --batch-size 32 --weights runs/train/exp179/weights/last.pt --cfg yolos.yaml --hyp data/hyp.scratch_bdd.yaml --resume
 
 python train.py --sdata ./data/bdd_daytime_fake.yaml --tdata ./data/bdd_night.yaml --epochs 200 --batch-size 32 --weights ./weights/yolov5s.pt --cfg yolos.yaml --hyp data/hyp.scratch_bdd.yaml
 python train.py --sdata ./data/bdd_daytime_fake.yaml --tdata ./data/bdd_night.yaml --epochs 200 --batch-size 32 --weights runs/train/exp88/weights/last.pt --cfg yolos.yaml --hyp data/hyp.scratch_bdd.yaml --resume
